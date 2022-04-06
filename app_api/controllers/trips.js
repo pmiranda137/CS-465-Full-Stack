d9f7750b79aa28428aecaf917d5b5c0c6f04995a
Randi <miranda.putnam@snhu.edu>
2022-04-06 11:10:51 -0400

    SPA

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -43,7 +43,70 @@ const tripsFindCode = async (req, res) => {
         });
 };
 
+const tripsAddTrip = async (req, res) => {
+    Trip
+    .create({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    },
+    (err, trip) => {
+        if (err){
+            return res
+                .status(400)
+                .json(err);
+        } else {
+            return res
+                .status(201)
+                .json(trip);
+        }
+    });
+}
+
+const tripsUpdateTrip = async (req, res) => {
+    console.log(req.body);
+    Trip
+    .findOneAndUpdate({ 'code': req.params.tripCode }, {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    }, { new: true })
+    .then(trip => {
+        if (!trip) {
+             return res
+                .status(404)
+                .send({
+                     message: "Trip not found with code " + req.params.tripCode
+                 });
+         }
+        res.send(trip);
+    }).catch(err => {
+        if (err.kind === 'ObjectId') {
+             return res
+                .status(404)
+                .send({
+                    message: "Trip not found with code " + req.params.tripCode
+                });
+        }
+        return res
+            .status(500) // server error
+            .json(err);
+        });
+}
+
 module.exports = {
     tripsList,
-    tripsFindCode
+    tripsFindCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,9 +5,11 @@ const tripsController = require('../controllers/trips');
 
 router
     .route('/trips')
-    .get(tripsController.tripsList);
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip);
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindCode);
+    .get(tripsController.tripsFindCode)
+    .put(tripsController.tripsUpdateTrip);
     
 module.exports = router;
